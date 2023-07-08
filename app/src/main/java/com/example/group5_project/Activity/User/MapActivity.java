d047a4b92ed010328d3c9c708063f2726080b7fe
lyngo-ntkl <lyngo.ntkl@gmail.com>
2023-07-08 10:34:51 +0700

    get device location

--- app/src/main/java/com/example/group5_project/Activity/User/MapActivity.java
+++ app/src/main/java/com/example/group5_project/Activity/User/MapActivity.java
@@ -9,7 +9,6 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Looper;
 import android.provider.Settings;
 import android.util.Log;
@@ -22,9 +21,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
 import com.example.group5_project.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -35,15 +37,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.maps.DirectionsApiRequest;
 import com.google.maps.GeoApiContext;
 import com.google.maps.PendingResult;
 import com.google.maps.internal.PolylineEncoding;
 import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.DirectionsRoute;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,13 +51,13 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final LatLng BRANCH = new LatLng(10.841203842437997, 106.80988558901014);
     private static final float MAP_ZOOM_DEFAULT_SCALE = 15;
     private static final int LOCATION_REQUEST_PERMISSION_CODE = 10;
-    private static final int GPS_REQUEST_PERMISSION_CODE = 11;
-    private ImageButton btnCurrentLocation;
     private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private com.google.maps.model.LatLng currentLocationLatLng;
+    private ImageButton btnCurrentLocation;
     private GoogleMap map;
     private GeoApiContext geoApiContext;
-    private Location currentLocation;
-    private List<Polyline> polylines = null;
     private boolean locationPermissionGranted = false;
 
     @Override
@@ -69,7 +69,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         btnCurrentLocation.setOnClickListener(this::onClick);
         SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         supportMapFragment.getMapAsync(this::onMapReady);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         geoApiContext = new GeoApiContext.Builder().apiKey(getString(R.string.google_maps_api_key)).build();
     }
 
@@ -123,9 +122,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     private void showGPSAlert(){
-        LocationRequest locationRequest = new LocationRequest.Builder(10000000).build();
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
-        builder.setAlwaysShow(true);
         AlertDialog dialog = new AlertDialog.Builder(this)
                 .setMessage("We needs GPS to show you direction, do you want to enable it?")
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -150,44 +146,42 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = new LocationRequest.Builder(10000000).build();
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        locationCallback = new LocationCallback() {
             @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                Location location = task.getResult();
-                Toast.makeText(MapActivity.this, location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
-            }
-        });
-//        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-//            @Override
-//            public void onSuccess(Location location) {
-//                if (location != null){
-//                    currentLocation = location;
-//                    Toast.makeText(MapActivity.this, location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
-//                    calculateDirections();
-//                }
-//            }
-//        });
-    }
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                fusedLocationProviderClient.removeLocationUpdates(this);
 
-    private String getDirectionURL(LatLng origin, LatLng destination){
-        String originParameter = "origin=" + origin.latitude + "," + origin.longitude;
-        String destinationParameter = "destination=" + destination.latitude + "," + destination.longitude;
-        String parameters = originParameter + "&" + destinationParameter;
-        String outputFormat = "json";
-        // can define transport mode
-        String url = "https://maps.googleapis.com/maps/api/directions/" + outputFormat + "?" + parameters + "key=AIzaSyAqaHEp1COlnSMCKPc2SW5OM6lkocN5u9Q";
-        return url;
+                if (locationResult != null && locationResult.getLocations().size() >0){
+                    int index = locationResult.getLocations().size() - 1;
+                    double latitude = locationResult.getLocations().get(index).getLatitude();
+                    double longitude = locationResult.getLocations().get(index).getLongitude();
+                    currentLocationLatLng = new com.google.maps.model.LatLng(latitude, longitude);
+                    Toast.makeText(MapActivity.this,"Latitude: "+ latitude + "\n" + "Longitude: "+ longitude, Toast.LENGTH_LONG).show();
+                    calculateDirections();
+                }
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
     }
 
     private void calculateDirections(){
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
+//        new Handler(Looper.getMainLooper()).post(new Runnable() {
+//            @Override
+//            public void run() {
                 DirectionsApiRequest directionsRequest = new DirectionsApiRequest(geoApiContext);
                 // show all possible route instead of the shortest (?) one
                 directionsRequest.alternatives(true);
-                directionsRequest.origin(new com.google.maps.model.LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()));
-                directionsRequest.destination(new com.google.maps.model.LatLng(BRANCH.latitude, BRANCH.longitude)).setCallback(new PendingResult.Callback<DirectionsResult>() {
+                directionsRequest.origin(currentLocationLatLng);
+                directionsRequest.destination(new com.google.maps.model.LatLng(BRANCH.latitude, BRANCH.longitude));
+                directionsRequest.setCallback(new PendingResult.Callback<DirectionsResult>() {
                     @Override
                     public void onResult(DirectionsResult result) {
                         addDirectionPolylines(result);
@@ -198,8 +192,20 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                         Log.e("", "Error at MapActivity: " + e.getMessage());
                     }
                 });
-            }
-        });
+
+//                        .setCallback(new PendingResult.Callback<DirectionsResult>() {
+//                            @Override
+//                            public void onResult(DirectionsResult result) {
+//                                addDirectionPolylines(result);
+//                            }
+//
+//                            @Override
+//                            public void onFailure(Throwable e) {
+//                                Log.e("", "Error at MapActivity: " + e.getMessage());
+//                            }
+//                        });
+//            }
+//        });
     }
 
     private void addDirectionPolylines(DirectionsResult result){
